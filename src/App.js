e3477c269f7f3956eede80bfd996464624e5b089
Nishant <nishants440@gmail.com>
2021-03-13 14:39:34 +0000

    Weather App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
+import { Dimmer, Loader } from 'semantic-ui-react';
+import Weather from './components/weather';
+export default function App() {
+  
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
 
-function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+
+      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+      .then(res => res.json())
+      .then(result => {
+        setData(result)
+        console.log(result);
+      });
+    }
+    
+    fetchData();
+  }, [lat,long])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(typeof data.main != 'undefined') ? (
+        <Weather weatherData={data}/>
+      ): (
+        <div>
+          <Dimmer active>
+            <Loader>Loading..</Loader>
+          </Dimmer>
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
---
+++ src/components/weather.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './styles.css';
+import moment from 'moment';
+import { Button } from 'semantic-ui-react';
+
+const refresh = () => {
+  window.location.reload();
+}
+
+const WeatherCard = ({weatherData}) => (
+  <div className="main">
+
+      <div className="top">
+        <p className="header">{weatherData.name}</p>
+        <Button className="button" inverted color='blue' circular icon='refresh' onClick={refresh} />
+      </div>
+      <div className="flex">
+        <p className="day">{moment().format('dddd')}, <span>{moment().format('LL')}</span></p>
+        <p className="description">{weatherData.weather[0].main}</p>
+      </div>
+
+      <div className="flex">
+        <p className="temp">Temprature: {weatherData.main.temp} &deg;C</p>
+        <p className="temp">Humidity: {weatherData.main.humidity} %</p>
+      </div>
+
+      <div className="flex">
+        <p className="sunrise-sunset">Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
+        <p className="sunrise-sunset">Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
+      </div>
+    
+  </div>
+)
+
+export default WeatherCard;
